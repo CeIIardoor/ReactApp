65b2dd683d905fa527f97b6a9f63358c33b7eb51
CeIIardoor <cellardoorweb@gmail.com>
2023-01-12 11:23:17 +0100

    Redux

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,28 +1,16 @@
 import Menu from './MenuComponent';
-import DISHES from '../shared/dishes.js';
 import { Component } from 'react';
-// import DishDetail from './DishdetailComponent.jsx';
 import Header from './HeaderComponent.jsx';
 import Footer from './FooterComponent.jsx';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import COMMENTS from '../shared/comments.js';
-import PROMOTIONS from '../shared/promotions.js';
-import LEADERS from '../shared/leaders.js';
 import DishDetail from './DishdetailComponent.jsx';
 import About from './AboutComponent.jsx';
 
 class MainComponent extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS,
-    };
   }
 
   render() {
