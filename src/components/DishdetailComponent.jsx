306b5ac4a5e1a3c6476589a682cb2fc66558e7dd
Cellardoor <cellardoorweb@gmail.com>
2023-01-26 12:44:33 +0000

    prod fix

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -17,7 +17,6 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
-import { baseUrl } from "../shared/baseUrl.js";
 import { Loading } from "./LoadingComponent.jsx";
 
 const required = val => val && val.length;
@@ -128,7 +127,7 @@ function RenderDish({ dish }) {
   return (
     <div className="col-12 col-md-5 m-1">
       <Card>
-        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+        <CardImg top src={dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
